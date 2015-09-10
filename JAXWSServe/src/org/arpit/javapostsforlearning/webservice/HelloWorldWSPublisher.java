51a5f2efe7743a08ff5a079fa46334ef2a6c371b
willian <fatima@fatima-PC>
2015-09-09 21:23:38 -0300

    Projeto referente ao Service

---
+++ JAXWSServe/src/org/arpit/javapostsforlearning/webservice/HelloWorldWSPublisher.java
@@ -0,0 +1,13 @@
+package org.arpit.javapostsforlearning.webservice;
+
+import javax.xml.ws.Endpoint;
+
+public class HelloWorldWSPublisher {
+
+	public static void main(String[] args) {
+
+		Endpoint.publish("http://localhost:8080/WS/HelloWorld",
+				new HelloWorldImpl());
+
+	}
+}
