2651921f436961780c567c1b02da85eb79d881aa
itec-desenv-willian <itec-desenv-willian@10.172.192.178>
2015-10-20 09:29:59 -0200

    comentário do link de acesso ao wsdl

--- JAXWSServe/src/org/arpit/javapostsforlearning/webservice/HelloWorldWSPublisher.java
+++ JAXWSServe/src/org/arpit/javapostsforlearning/webservice/HelloWorldWSPublisher.java
@@ -5,7 +5,7 @@ import javax.xml.ws.Endpoint;
 public class HelloWorldWSPublisher {
 
 	public static void main(String[] args) {
-
+		//http://localhost:8080/WS/HelloWorld?wsdl
 		Endpoint.publish("http://localhost:8080/WS/HelloWorld",
 				new HelloWorldImpl());
 
